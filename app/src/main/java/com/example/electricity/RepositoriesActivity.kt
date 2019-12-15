fa7549faf623f77df8377c00a6342593b751e9d1
Cagin Akgül <cagin.akgul@hotmail.com>
2019-12-15 21:24:02 +0100

    Renaming activity

--- app/src/main/java/com/example/electricity/RepositoriesActivity.kt
+++ app/src/main/java/com/example/electricity/RepositoriesActivity.kt
@@ -3,10 +3,10 @@ package com.example.electricity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RepositoriesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_repositories)
     }
 }
