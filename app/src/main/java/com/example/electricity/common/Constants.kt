59e0d52b1338124b09fbddf353f25bcf9d5b967f
Cagin Akgül <cagin.akgul@hotmail.com>
2019-12-17 17:21:27 +0100

    Adding Repository pattern.

--- app/src/main/java/com/example/electricity/common/Constants.kt
+++ app/src/main/java/com/example/electricity/common/Constants.kt
@@ -2,6 +2,6 @@ package com.example.electricity.common
 
 class Constants {
     object Api {
-        val BASE_URL = "https://qc05n0gp78.execute-api.eu-central-1.amazonaws.com/prod/"
+        const val BASE_URL = "https://api.github.com/"
     }
 }
--- app/src/main/java/com/example/electricity/network/GithubRepository.kt
+++ app/src/main/java/com/example/electricity/network/GithubRepository.kt
@@ -1,4 +1,8 @@
 package com.example.electricity.network
 
+import androidx.lifecycle.LiveData
+import com.example.electricity.network.data.RepositoryDetails
+
 interface GithubRepository {
+    fun fetchPopularRepositories(query: String): LiveData<List<RepositoryDetails>>
 }
