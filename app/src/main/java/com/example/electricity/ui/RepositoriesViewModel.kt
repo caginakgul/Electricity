9fea2f1e33a3636ae885f930300c907b5237e8af
Cagin Akgül <cagin.akgul@hotmail.com>
2019-12-18 17:31:19 +0100

    Cleaning unused lines

--- app/src/main/java/com/example/electricity/ui/RepositoriesViewModel.kt
+++ app/src/main/java/com/example/electricity/ui/RepositoriesViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.electricity.ui
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.electricity.base.BaseViewModel
 import com.example.electricity.network.GithubRepository
 import com.example.electricity.network.data.RepositoryDetails
@@ -10,13 +9,10 @@ import javax.inject.Inject
 class RepositoriesViewModel @Inject constructor(
     private val githubRepository: GithubRepository
 ) : BaseViewModel() {
-    val viewData: MutableLiveData<List<RepositoryDetails>> = MutableLiveData()
+    fun getNewsData(): LiveData<List<RepositoryDetails>> =
+        githubRepository.fetchPopularRepositories(QUERY_STRING)
 
-    fun getNewsData(): LiveData<List<RepositoryDetails>> {
-        return githubRepository.fetchPopularRepositories(QUERY_STRING)
-    }
-
-    private companion object{
+    private companion object {
         private const val QUERY_STRING = "stars:>25000"
     }
 }
