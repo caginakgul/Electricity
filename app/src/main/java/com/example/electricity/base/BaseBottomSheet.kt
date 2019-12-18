1a28418f26f7f7862b700a68d20da1515dc810b1
Cagin Akgül <cagin.akgul@hotmail.com>
2019-12-18 13:48:13 +0100

    Implementing bottom sheet fragment to display details

---
+++ app/src/main/java/com/example/electricity/base/BaseBottomSheet.kt
@@ -0,0 +1,15 @@
+package com.example.electricity.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+abstract class BaseBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = super.onCreateView(inflater, container, savedInstanceState)
+}
---
+++ app/src/main/java/com/example/electricity/ui/details/RepositoryDetailsBottomSheet.kt
@@ -0,0 +1,51 @@
+package com.example.electricity.ui.details
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.electricity.R
+import com.example.electricity.base.BaseBottomSheet
+import com.example.electricity.common.Constants
+import com.example.electricity.network.data.RepositoryDetails
+import kotlinx.android.synthetic.main.repository_bottom_sheet.*
+
+class RepositoryDetailsBottomSheet : BaseBottomSheet() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.repository_bottom_sheet, container,
+            false
+        )
+    }
+
+    private fun initData() {
+        val args = arguments
+        val repositoryDetailsData =
+            args?.getParcelable<RepositoryDetails>(Constants.Data.REPOSITORY_ITEM_KEY)
+        projectNameTextViewTitle.text = repositoryDetailsData?.name ?: ""
+        idTextViewDynamic.text = repositoryDetailsData?.id.toString()
+        urlTextViewDynamic.text = repositoryDetailsData?.html_url ?: ""
+        starTextViewDynamic.text = repositoryDetailsData?.stargazersCount.toString()
+        issueTextViewDynamic.text = repositoryDetailsData?.openIssuesCount.toString()
+    }
+
+    companion object {
+        fun newInstance(repo: RepositoryDetails): RepositoryDetailsBottomSheet {
+            val args = Bundle()
+            args.putParcelable(Constants.Data.REPOSITORY_ITEM_KEY, repo)
+            val fragment = RepositoryDetailsBottomSheet()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initData()
+    }
+}
